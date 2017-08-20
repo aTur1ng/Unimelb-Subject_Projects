4a6ae6c8466f080d7f3e3bc8f74d9c2d0c1adf1e
aTur1ng <zc.chen@protonmail.com>
2017-08-20 17:00:48 +1000

    Add Assignment1 files

---
+++ COMP90048_2017_SM2/Assignment1/Lab1.hs
@@ -0,0 +1,22 @@
+-- @author CHEN Zhengcheng 725439
+
+module Lab1 (subst, interleave, unroll) where
+
+subst :: Eq t => t -> t -> [t] -> [t]
+subst a b [] = []
+subst a b (x:xs) 
+  | x == a = (b:subst a b xs)
+  | otherwise = (x:subst a b xs)
+    
+interleave :: [t] -> [t] -> [t]
+interleave [] [] = []
+interleave l1 [] = l1
+interleave [] l2 = l2
+interleave (x:xs) (y:ys) = (x:y:interleave xs ys)
+
+unroll :: Int -> [a] -> [a]
+unroll 0 _ = []
+unroll _ [] = []
+unroll n l 
+	 | n > length l = take n (cycle l)
+	 | otherwise = take n l
